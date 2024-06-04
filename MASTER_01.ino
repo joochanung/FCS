fa0bef31f1bad884eb64892889af64e03eb6bc6b
GyeongminJang <166125657+GyeongminJang@users.noreply.github.com>
2024-06-04 12:51:56 +0900

    Update MASTER_01.ino
    LASERPIN : A0 -> 0으로 수정.
    
    A0로 설정하면 레이저 모듈이 안 켜져서 0으로 해서 켜지도록 함.

--- MASTER_01.ino
+++ MASTER_01.ino
@@ -3,10 +3,11 @@
 #include <SoftwareSerial.h>
 
 // #define BUZZERPIN A0
-#define LASERPIN A0
+#define LASERPIN 0
 #define BTRXD A2
 #define BTTXD A3
-
+#define BUZZER_INT PD2
+#define BUZZERPWM PD3
 #define SCK PB5
 #define MISO PB4
 #define MOSI PB3
@@ -18,9 +19,6 @@
 #define IN4 PD4
 #define IN3 PD5
 #define ENB PD6
-
-const int buzzerPin = A0;
-const int inputPin = 2;
 
 SoftwareSerial btserial(BTTXD, BTRXD); // 블루투스 핀 설정
 
@@ -37,31 +35,14 @@ void setup() {
   btserial.begin(9600);
   Serial.begin(9600);
 
-  // 모터 핀을 출력 모드로 설정
-  DDRB |= (1 << ENA) | (1 << IN1);
-  DDRD |= (1 << IN2) | (1 << ENB) | (1 << IN3) | (1 << IN4);
-
-  // 초기 값 설정
-  PORTB &= ~(1 << IN1); // in1Pin = LOW
-  PORTD &= ~(1 << IN2); // in2Pin = LOW
-  PORTD &= ~(1 << IN3); // in3Pin = LOW
-  PORTD &= ~(1 << IN4); // in4Pin = LOW
-  OCR1A = 0; // ENA PWM 초기값 0
-  OCR0A = 0; // ENB (PD6) PWM 초기값 0
-
-  // PWM 설정
-  // TIMER1 세팅 (MOTOR_A)
-  TCCR1A |= (1 << COM1A1); // 비반전 모드
-  TCCR1A |= (1 << WGM11) | (1 << WGM10); // Fast PWM, 8비트
-  TCCR1B |= (1 << WGM12) | (1 << CS11); // 분주비 8
-
-  // TIMER0 세팅 (MOTOR_B)
-  TCCR0A |= (1 << COM0A1); // 비반전 모드
-  TCCR0A |= (1 << WGM01) | (1 << WGM00); // Fast PWM, 8비트
-  TCCR0B |= (1 << CS01); // 분주비 8
+  // DC 모터 초기 설정
+  Motor_init();
 
   // 레이저 모듈
-  DDRC |= (1 << LASERPIN); 
+  /*
+  pinMode(LASERPIN,OUTPUT);
+  digitalWrite(LASERPIN,HIGH); */
+  DDRC |= (1 << LASERPIN);
   PORTC &= ~(1 << LASERPIN);
 }
 
@@ -91,7 +72,7 @@ void loop() {
   }
 }
 
-// Function 단계
+// 데이터 전송 단계
 void sendDataToSlave(int data) {
   byte highByte = (data >> 8) & 0xFF; // Upper 8 bits
   byte lowByte = data & 0xFF;         // Lower 8 bits
@@ -99,17 +80,42 @@ void sendDataToSlave(int data) {
   // Select slave
   PORTB &= ~(1 << PB2);
 
-  // Transfer high and low bytes
+  // high & low bytes 변환
   SPDR = highByte;
-  while (!(SPSR & (1 << SPIF))) ; // Wait for transmission complete
+  while (!(SPSR & (1 << SPIF))) ; // 다른 변환 완료까지 대기
   SPDR = lowByte;
-  while (!(SPSR & (1 << SPIF))) ; // Wait for transmission complete
+  while (!(SPSR & (1 << SPIF))) ; // 다른 변환 완료까지 대기
   
   // Deselect slave
   PORTB |= (1 << PB2);
 
   Serial.print("Sent data: ");
   Serial.println(data);
+}
+
+void Motor_init(){
+  // 모터 핀을 출력 모드로 설정
+  DDRB |= (1 << ENA) | (1 << IN1);
+  DDRD |= (1 << IN2) | (1 << ENB) | (1 << IN3) | (1 << IN4);
+
+  // 초기 값 설정
+  PORTB &= ~(1 << IN1); // in1Pin = LOW
+  PORTD &= ~(1 << IN2); // in2Pin = LOW
+  PORTD &= ~(1 << IN3); // in3Pin = LOW
+  PORTD &= ~(1 << IN4); // in4Pin = LOW
+  OCR1A = 0; // ENA PWM 초기값 0
+  OCR0A = 0; // ENB (PD6) PWM 초기값 0
+
+  // PWM 설정
+  // TIMER1 세팅 (MOTOR_A)
+  TCCR1A |= (1 << COM1A1); // 비반전 모드
+  TCCR1A |= (1 << WGM11) | (1 << WGM10); // Fast PWM, 8비트
+  TCCR1B |= (1 << WGM12) | (1 << CS11); // 분주비 8
+
+  // TIMER0 세팅 (MOTOR_B)
+  TCCR0A |= (1 << COM0A1); // 비반전 모드
+  TCCR0A |= (1 << WGM01) | (1 << WGM00); // Fast PWM, 8비트
+  TCCR0B |= (1 << CS01); // 분주비 8
 }
 
 void DCmotorON() {
@@ -135,3 +141,4 @@ void DCmotorOFF() {
   PORTD &= ~(1 << PD4); // in4Pin = LOW
   OCR0A = 0;   
 }
+
